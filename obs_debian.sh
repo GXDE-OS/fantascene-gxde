91049469cf9c527f4bbc797f101fa25526093d99
shenmo <jifengshenmo@outlook.com>
2024-10-05 19:42:04 +0800

    Now we depend on mainline version

--- obs_debian.sh
+++
@@ -1,8 +0,0 @@
-git clone --depth=1 https://gitee.com/liuminghang/fantascene-dynamic-wallpaper.git
-tar -czvf fantascene-dynamic-wallpaper_1.8.3.orig.tar.gz ./fantascene-dynamic-wallpaper/
-cd fantascene-dynamic-wallpaper/
-tar -czvf ../fantascene-dynamic-wallpaper_1.8.3.debian.tar.gz ./debian
-cd ..
-md5sum fantascene-dynamic-wallpaper_1.8.3.orig.tar.gz
-md5sum fantascene-dynamic-wallpaper_1.8.3.debian.tar.gz
-ls -l
